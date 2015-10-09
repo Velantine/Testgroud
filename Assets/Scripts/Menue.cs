ed6ecafaeb47dc63a3611f2e10b731636ca4e97a
ROisAWESOME <robletsplays1@googlemail.com>
2015-10-09 23:40:45 +0200

    Networkamanagerfix

--- Assets/Scripts/Menue.cs
+++ Assets/Scripts/Menue.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 public class Menue : MonoBehaviour {
 	public Text newsText;
 	public string urlNews = "http://roisawesome.bplaced.net/Testbuild/news.txt";
+	GameObject Networkmanager;
 	// Use this for initialization
 
 	IEnumerator Start() {
 		WWW www = new WWW(urlNews);
 		yield return www;
 		newsText.text = www.text;
+		DestroyImmediate (GameObject.Find ("Networkmanager"));
+		
+
 	}
 	
 
