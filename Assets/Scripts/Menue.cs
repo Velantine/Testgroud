e1a073f20cd5d446814ca2cca3f3e7b5ba419873
ROisAWESOME <robletsplays1@googlemail.com>
2015-10-09 18:11:33 +0200

    Menü
    Lädt News vom Webspace

--- Assets/Scripts/Menue.cs
+++ Assets/Scripts/Menue.cs
@@ -3,12 +3,17 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Menue : MonoBehaviour {
-
+	public Text NewsText;
+	public string urlNews = "http://roisawesome.bplaced.net/Testbuild/news.txt";
 	// Use this for initialization
-	void Start () {
-		
+
+	IEnumerator Start() {
+		WWW www = new WWW(urlNews);
+		yield return www;
+		NewsText.text = www.text;
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 	
