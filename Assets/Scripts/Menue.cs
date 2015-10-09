66ca6b7de7a483a8427bb4640c0579a05e98cd84
ROisAWESOME <robletsplays1@googlemail.com>
2015-10-09 18:14:32 +0200

    Menüfix2

--- Assets/Scripts/Menue.cs
+++ Assets/Scripts/Menue.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Menue : MonoBehaviour {
-	public Text NewsText;
+	public Text newsText;
 	public string urlNews = "http://roisawesome.bplaced.net/Testbuild/news.txt";
 	// Use this for initialization
 
 	IEnumerator Start() {
 		WWW www = new WWW(urlNews);
 		yield return www;
-		NewsText.text = www.text;
+		newsText.text = www.text;
 	}
 	
 
