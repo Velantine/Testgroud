14d5c782e21e3a9cc340d579c1ea9992902f694b
ROisAWESOME <robletsplays1@googlemail.com>
2015-10-12 20:24:05 +0200

    Network Player

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+
+    public GameObject Camera;
+    public GameObject Head;
+    public GameObject FirstpersonGun;
+    public GameObject ThirdpersonGun;
+
+    [HideInInspector]
+    public NetworkMovement movement;
+
+    [HideInInspector]
+    public CharacterController CharacterController;
+
+    void Awake()
+    {
+        movement = GetComponent<NetworkMovement>();
+    }
+
+    void Start()
+    {
+        CharacterController = this.GetComponent<CharacterController>();
+
+        this.GetComponent<FirstPersonController>().enabled = isLocalPlayer;
+        Camera.GetComponent<AudioListener>().enabled = isLocalPlayer;
+        Camera.GetComponent<Camera>().enabled = isLocalPlayer;
+        FirstpersonGun.SetActive(isLocalPlayer);
+        ThirdpersonGun.SetActive(!isLocalPlayer);
+    }
+
+}
