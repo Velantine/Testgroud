c9e7403e7d3c5d172e881761f60006a580d3836f
ROisAWESOME <robletsplays1@googlemail.com>
2015-10-29 21:57:13 +0100

    Respawn

--- Assets/Scripts/NetworkHealth.cs
+++ Assets/Scripts/NetworkHealth.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
 
 [NetworkSettings(channel = 1)]
 public class NetworkHealth : NetworkBehaviour
@@ -8,6 +9,10 @@ public class NetworkHealth : NetworkBehaviour
     [Range(0, 100)]
     public float Health = 100f;
 	public float deaths = 0f;
+
+	bool dead = false;
+	public GameObject RespawnB;
+
 
 
     //TODO: [Server]
@@ -24,10 +29,23 @@ public class NetworkHealth : NetworkBehaviour
     }
 
 	void Update(){
-		if(Health<=0f){
+		if(Health<=0f&&dead==false){
+			dead=true;
 			print("Somebody died!");
+			gameObject.GetComponent<Collider>().enabled = false;
+			gameObject.GetComponent<FirstPersonController>().enabled = false;
+			gameObject.GetComponent<NetworkGun>().enabled = false;
+			RespawnB.SetActive(true);
 			deaths++;
-			Health=100f;
 		}
+	}
+	public void Respawn(){
+		RespawnB.SetActive(false);
+		gameObject.transform.position =  GameObject.Find ("Spawn").gameObject.transform.position;
+		gameObject.GetComponent<Collider>().enabled = true;
+		gameObject.GetComponent<FirstPersonController>().enabled = true;
+		gameObject.GetComponent<NetworkGun>().enabled = true;
+		Health=100f;
+		dead = false;
 	}
 }
