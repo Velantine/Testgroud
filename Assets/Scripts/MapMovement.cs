93938ce2d86900fa3b5a5198ca533bcfb4d229f1
ROisAWESOME <robletsplays1@googlemail.com>
2015-12-04 20:02:32 +0100

    MapMovement

---
+++ Assets/Scripts/MapMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MapMovement : NetworkBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(isLocalPlayer){
+		Vector2 movement = new Vector2 (Input.GetAxis("Horizontal")*speed*Time.deltaTime,Input.GetAxis("Vertical")*speed*Time.deltaTime);
+		this.transform.Translate (movement);
+		//}
+	}
+}
