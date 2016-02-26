c6103179b57f6ac20fb3aca5b6cae09f1dde2b59
ROisAWESOME <robletsplays1@googlemail.com>
2016-02-26 20:09:31 +0100

    Fixes
    No Cursor, Map enhancement, etc

---
+++ Assets/Scripts/CoursurOnLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoursurOnLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+}
