5605282e5d212f8397adee5e99fd6a3583e9e278
ROisAWESOME <robletsplays1@googlemail.com>
2016-03-07 20:11:08 +0100

    mähhh

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -41,33 +41,36 @@ public class LookAt : NetworkBehaviour {
 				case "Vehicle":
 					//Something
 					break;
-				case "Activate":
-					if (Input.GetButtonDown ("Enter")) {
-						pickUp.Play ();
-						if (hit.transform.gameObject.GetComponent<ObInfo> ().name == "SpawnAmmo_Box") {
-							//GameObject ammoBox = (GameObject)Instantiate (prefabs [0], new Vector3 (hit.transform.position.x, hit.transform.position.y, hit.transform.position.z-2), Quaternion.identity);
-							//NetworkServer.Spawn(ammoBox);
-							CmdSpawn(0,hit.transform.gameObject);
-							break;
-						}
-						if (hit.transform.gameObject.GetComponent<ObInfo> ().name == "SpawnMedkit") {
-							//Network.Instantiate (prefabs[1], new Vector3(hit.transform.position.x,hit.transform.position.y,hit.transform.position.z-2), transform.rotation, 0);
-							CmdSpawn(1,hit.transform.gameObject);
-							break;
-						}
-						/*if (hit.transform.gameObject.GetComponent<ObInfo> ().name == "Ship") {
-							Instantiate (prefabs [2], new Vector3 (hit.transform.position.x, hit.transform.position.y+20, hit.transform.position.z), Quaternion.identity);
-							break;
-						}*/
+			case "Activate":
+				if (Input.GetButtonDown ("Enter")) {
+					pickUp.Play ();
+					if (hit.transform.gameObject.GetComponent<ObInfo> ().name == "SpawnAmmo_Box") {
+						//GameObject ammoBox = (GameObject)Instantiate (prefabs [0], new Vector3 (hit.transform.position.x, hit.transform.position.y, hit.transform.position.z-2), Quaternion.identity);
+						//NetworkServer.Spawn(ammoBox);
+						CmdSpawn (0, hit.transform.gameObject);
+						break;
+					}
+					if (hit.transform.gameObject.GetComponent<ObInfo> ().name == "SpawnMedkit") {
+						//Network.Instantiate (prefabs[1], new Vector3(hit.transform.position.x,hit.transform.position.y,hit.transform.position.z-2), transform.rotation, 0);
+						CmdSpawn (1, hit.transform.gameObject);
+						break;
+					}
+						//if(){}
+						
 					}
 					break;
-			}
+							}
 		}
 	}
 
 	[Command]
 	void CmdSpawn(int pre, GameObject pos){
-		GameObject objectToSpawn = (GameObject)Instantiate (prefabs [pre], new Vector3 (pos.transform.position.x, pos.transform.position.y, pos.transform.position.z-2), Quaternion.identity);
-		NetworkServer.Spawn(objectToSpawn);
+		if (isLocalPlayer) {
+			GameObject objectToSpawn = (GameObject)Instantiate (prefabs [pre], new Vector3 (pos.transform.position.x, pos.transform.position.y, pos.transform.position.z - 2), Quaternion.identity);
+			NetworkServer.Spawn(objectToSpawn);
+		} else {
+			GameObject objectToSpawn = (GameObject)Instantiate (prefabs [pre], gameObject.transform.position, Quaternion.identity);
+			NetworkServer.Spawn(objectToSpawn);
+		}
 	}
 }
