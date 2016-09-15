61a0081ae4b7257572c5ec78431c53e7fd4340fa
ROisAWESOME <robletsplays1@googlemail.com>
2016-09-15 21:40:49 +0200

    Main Menu partially implemented
    -Credits
    -Play
    -Quit
    -Options (Keymapper, Grafics, Audio)

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+    public void Scene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
